bac7b27f430f735e9974d48c835365bb8b5f071d
Symsystem <symeon.delmarmol@gmail.com>
2015-03-28 05:30:35 +0100

    GPSTracker initialize

--- app/src/main/java/com/hackathon/handiplace/activities/MenuActivity.java
+++ app/src/main/java/com/hackathon/handiplace/activities/MenuActivity.java
@@ -1,6 +1,10 @@
 package com.hackathon.handiplace.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.hackathon.handiplace.R;
+import com.hackathon.handiplace.request.GPSTracker;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -29,13 +34,26 @@ public class MenuActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         ButterKnife.inject(this);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        configureLocation();
+    }
+
+    private void configureLocation() {
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // Define a listener that responds to location updates
+        GPSTracker locationListener = new GPSTracker();
+
+        // Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
     }
 
     @OnClick (R.id.restoLocationButton)
