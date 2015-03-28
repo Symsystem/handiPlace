b5f21507397e6332dd665640e13f2380cebee4d9
jeremyduchesne <duchesnejeremy@gmail.com>
2015-03-28 23:54:28 +0100

    Ajouté CommentActivity

--- app/src/main/java/com/hackathon/handiplace/activities/CommentActivity.java
+++ app/src/main/java/com/hackathon/handiplace/activities/CommentActivity.java
@@ -1,40 +1,104 @@
 package com.hackathon.handiplace.activities;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-import com.hackathon.handiplace.R;
+ import android.os.Bundle;
+        import android.support.v7.app.ActionBarActivity;
+        import android.app.AlertDialog;
+        import android.content.Intent;
+        import android.support.v7.app.ActionBarActivity;
+        import android.os.Bundle;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.TextView;
+        import android.widget.Toast;
+        import com.android.volley.RequestQueue;
+        import com.android.volley.Response;
+        import com.android.volley.VolleyError;
+        import com.android.volley.toolbox.Volley;
+        import com.hackathon.handiplace.HandiPlaceApplication;
+        import com.hackathon.handiplace.R;
+        import com.hackathon.handiplace.classes.Utils;
+        import com.hackathon.handiplace.request.OkHttpStack;
+        import com.hackathon.handiplace.request.PostRequest;
+        import org.json.JSONArray;
+        import org.json.JSONException;
+        import org.json.JSONObject;
+        import java.util.HashMap;
+        import java.util.Map;
 
 public class CommentActivity extends ActionBarActivity {
+
+    private TextView mTextView;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment);
-    }
 
+    mTextView = (TextView) findViewById(R.id.editText);
+    mButton = (Button) findViewById(R.id.button);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_comment, menu);
-        return true;
-    }
+    mButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v){
+            String comment = mTextView.getText().toString().trim();
+            if(comment.isEmpty()) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(CommentActivity.this);
+                builder.setTitle("Erreur");
+                builder.setMessage("Vous n'avez pas écrit de commentaire");
+                builder.setPositiveButton(android.R.string.ok, null);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+            else {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("content", comment);
+                params.put("idUser", HandiPlaceApplication.user.getId() + "");
+                String URL = Utils.BASE_URL + "api/comments/{idPlace}.JSON";
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+                PostRequest requestAddUser = new PostRequest(URL, params, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        try{
+                            JSONObject userJSON = new JSONObject(s);
+                            if (userJSON.has("response")) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                                if (userJSON.getBoolean("response")) {
+
+                                }
+                                else {
+                                    // erreur
+                                }
+                            } else {
+                                // erreur
+                            }
+                        }
+                        catch(JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError volleyError) {
+
+                            }
+                        });
+                RequestQueue queue = Volley.newRequestQueue(CommentActivity.this, new OkHttpStack());
+                queue.add(requestAddUser);
+            }
+            newActivity();
         }
 
-        return super.onOptionsItemSelected(item);
+    });
+
+
+}
+    private void newActivity(){
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
     }
 }
