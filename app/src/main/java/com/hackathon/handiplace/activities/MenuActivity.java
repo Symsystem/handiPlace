9fd17199b8f418a7fb0b433d4523684b6f029ce6
Symsystem <symeon.delmarmol@gmail.com>
2015-03-28 08:28:49 +0100

    LocationAsyncTask finished!

--- app/src/main/java/com/hackathon/handiplace/activities/MenuActivity.java
+++ app/src/main/java/com/hackathon/handiplace/activities/MenuActivity.java
@@ -1,15 +1,19 @@
 package com.hackathon.handiplace.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.hackathon.handiplace.R;
 import com.hackathon.handiplace.classes.Position;
-import com.hackathon.handiplace.request.LocationAsyncTask;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -25,6 +29,8 @@ public class MenuActivity extends ActionBarActivity {
     @InjectView(R.id.restoFavorisButton) Button mRestoFavoris;
     @InjectView(R.id.typeHandicapButton) Button mTypeHandicap;
 
+    private Position mPosition;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +40,7 @@ public class MenuActivity extends ActionBarActivity {
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-               getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         Position currentPosition;
         LocationAsyncTask loc = new LocationAsyncTask(this);
@@ -53,6 +59,41 @@ public class MenuActivity extends ActionBarActivity {
     public void startDisabledTypeActivity(View view){
         Intent intent = new Intent(this, DisabledTypeActivity.class);
         startActivity(intent);
+    }
+
+    public class LocationAsyncTask extends AsyncTask<Void, Void, Position> {
+
+        private Context context;
+
+        public LocationAsyncTask(Context context){
+            this.context = context;
+        }
+
+        @Override
+        protected void onPostExecute(Position result) {
+            Toast.makeText(context, "Position GOT !", Toast.LENGTH_LONG).show();
+            mPosition = result;
+        }
+
+        @Override
+        protected Position doInBackground(Void... params) {
+
+            // Acquire a reference to the system Location Manager
+            LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+
+            Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+            //locationManager.requestLocationUpdates(provider, 1000, 10, locationListener);
+
+            return new Position(location.getLatitude(), location.getLongitude());
+        }
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+
     }
 
 }
