a452b22e53220cd6f22b5a5f524fee41cf4634a3
Symsystem <symeon.delmarmol@gmail.com>
2015-03-28 21:52:14 +0100

    Ajout méthode pour vérifier la connexion GPS et internet

--- app/src/main/java/com/hackathon/handiplace/activities/MenuActivity.java
+++ app/src/main/java/com/hackathon/handiplace/activities/MenuActivity.java
@@ -7,7 +7,6 @@ import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -63,27 +62,7 @@ public class MenuActivity extends ActionBarActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        if(!Utils.isNetworkAvailable(this))
-        {
-            Log.d("Ceci n'est pas normal", "");
-            Intent intent = new Intent(MenuActivity.this, InternetActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-        }
-
-        if(!Utils.isLocationEnabled(this) && !HandiPlaceApplication.isLocated){
-            Intent intent = new Intent(MenuActivity.this, LocationActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-        }
-        else if(!HandiPlaceApplication.isLocated){
-            mPosition = Utils.getLocation(this);
-            if(mPosition != null){
-                HandiPlaceApplication.isLocated = true;
-            }
-        }
+        mPosition = Utils.checkConnectionsReturnLocation(this);
 
         if(!HandiPlaceApplication.user.isConnected()) {
             login();
@@ -248,6 +227,7 @@ public class MenuActivity extends ActionBarActivity {
 
     @OnClick (R.id.typeHandicapButton)
     public void startDisabledTypeActivity(View view){
+        Utils.checkConnectionsReturnLocation(this);
         Intent intent = new Intent(this, DisabledTypeActivity.class);
         startActivity(intent);
     }
