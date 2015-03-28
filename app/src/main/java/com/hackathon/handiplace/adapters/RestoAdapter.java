76a8d87a1bb6afbbaf0186af19d88a4cd06cea78
Symsystem <symeon.delmarmol@gmail.com>
2015-03-28 02:14:34 +0100

    debut adapter

--- app/src/main/java/com/hackathon/handiplace/adapters/RestoAdapter.java
+++ app/src/main/java/com/hackathon/handiplace/adapters/RestoAdapter.java
@@ -1,7 +1,12 @@
 package com.hackathon.handiplace.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.IconTextView;
+import android.widget.TextView;
 
 import com.hackathon.handiplace.R;
 import com.hackathon.handiplace.classes.Restaurant;
@@ -19,4 +24,45 @@ public class RestoAdapter extends ArrayAdapter<Restaurant> {
         this.restaurants = restaurants;
     }
 
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder holder;
+
+        if (convertView == null) {
+
+            convertView = LayoutInflater.from(context).inflate(R.layout.resto_list_item, null);
+            holder = new ViewHolder();
+            holder.name = (TextView) convertView.findViewById(R.id.resto_name);
+            holder.category = (TextView) convertView.findViewById(R.id.resto_cat);
+            holder.rating = (IconTextView) convertView.findViewById(R.id.resto_rating);
+            convertView.setTag(holder);
+
+        }
+
+        else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        Restaurant restaurant = restaurants.get(position);
+
+        String nameStr = bor.getName();
+        double mo = (Math.round(bor.getMoney() * 100.0) / 100.0);
+        String moneyStr = mo + LoanReminderApplication.currency;
+
+        holder.name.setText(nameStr);
+        holder.category.setText(moneyStr);
+
+        return convertView;
+
+    }
+
+    private static class ViewHolder {
+
+        TextView name;
+        TextView category;
+        IconTextView rating;
+
+    }
 }
--- app/src/main/java/com/hackathon/handiplace/classes/Restaurant.java
+++ app/src/main/java/com/hackathon/handiplace/classes/Restaurant.java
@@ -10,6 +10,56 @@ public class Restaurant {
     private String address;
     private String imageURL;
 
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    private int rating;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
     private ArrayList<Criterion> criterions;
 
     public Restaurant(String name, String description, String category, String address, ArrayList<Criterion> criterions) {
