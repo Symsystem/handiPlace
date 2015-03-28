aa28feecf4c04f10d0f102e03186de2bb632e5e5
Symsystem <symeon.delmarmol@gmail.com>
2015-03-28 02:35:23 +0100

    debut adapter

--- app/src/main/java/com/hackathon/handiplace/dummies/DummyData.java
+++ app/src/main/java/com/hackathon/handiplace/dummies/DummyData.java
@@ -10,10 +10,11 @@ public class DummyData {
 
         ArrayList<Restaurant> restaurants = new ArrayList<>();
         restaurants.add(new Restaurant("Au délice", "Venez vous régaler chez nous!", "Frites",
-                "Impasse de la Fidélité 4A 1000 Ville de Bruxelles", null));
+                "Impasse de la Fidélité 4A 1000 Ville de Bruxelles", 2, null));
         restaurants.add(new Restaurant("Pépé café", "Pour bien manger !", "Italien",
-                "Rue de Tabora 11 1000 Ville de Bruxelles", null));
-        restaurants.add(new Restaurant("Xu ji", "Venez vous régaler chez nous!", "Chinois", "Rue des Poissonniers 5 1000 Ville de Bruxelles", null));
+                "Rue de Tabora 11 1000 Ville de Bruxelles", 3, null));
+        restaurants.add(new Restaurant("Xu ji", "Venez vous régaler chez nous!", "Chinois",
+                "Rue des Poissonniers 5 1000 Ville de Bruxelles", 1, null));
 
     }
 
