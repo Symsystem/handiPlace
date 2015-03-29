5ca3278be3228cfa971ddb792c92f536677eb9df
Quentin <lombatquentin@live.fr>
2015-03-29 12:23:20 +0200

    Location correction

--- app/src/main/java/com/hackathon/handiplace/activities/RestoDetailsActivity.java
+++ app/src/main/java/com/hackathon/handiplace/activities/RestoDetailsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.IconTextView;
@@ -80,6 +81,15 @@ public class RestoDetailsActivity extends ActionBarActivity {
     private IconTextView[] stars;
     int selected;
     Toolbar toolbar;
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                RestoDetailsActivity.this.finish();
+        }
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
